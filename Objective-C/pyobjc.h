07272baa7e714d2f1c2f1a7ba63bfc2317967fab
Matthias Benkard <code@mail.matthias.benkard.de>
2007-09-18 17:35:23 +0200

    Objective-C layer: Make up for forgetting to add a replacement for pyobjc.h.
    darcs-hash:0760998bd29643bfe0ccca1eb65d8022b2d93390

---
+++ Objective-C/pyobjc.h
@@ -0,0 +1,46 @@
+/* Copyright 2007, Matthias Andreas Benkard. */
+
+#ifndef __pyobjc_H
+#define __pyobjc_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libobjcl.h"
+
+
+#ifdef __NEXT_RUNTIME__
+
+#ifndef APPLE_RUNTIME
+#define APPLE_RUNTIME
+#endif
+
+#import <objc/objc-class.h>
+
+#else /* !__NEXT_RUNTIME__ */
+
+#ifndef GNU_RUNTIME
+#define GNU_RUNTIME
+#endif
+
+#import <objc/objc-api.h>
+#ifndef __CXX__
+#define bool BOOL
+#endif
+
+#endif /* __NEXT_RUNTIME__ */
+
+
+#define PyMem_Free free
+#define PyMem_Malloc malloc
+#define Py_ssize_t ssize_t
+
+#ifdef OOM_KILL
+#define PyErr_NoMemory() [objcl_oom_exception raise]
+#else
+#define PyErr_NoMemory() NSLog (@"ERROR: Memory exhausted.");
+#endif
+
+#include "objc_support.h"
+#include "libffi_support.h"
+
+#endif /* __pyobjc_H */
