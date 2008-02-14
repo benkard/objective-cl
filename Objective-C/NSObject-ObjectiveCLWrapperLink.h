b2c371b10aff7125ae5cbfd83ba643e77b1bfb87
Matthias Benkard <code@mail.matthias.benkard.de>
2008-02-14 21:42:48 +0100

    Objective-C layer: Track Lisp backing status by class instead of by instance.
    darcs-hash:0094d958feb2fd5e1715f953f6a67249850f095c

--- Objective-C/NSObject-ObjectiveCLWrapperLink.h
+++ Objective-C/NSObject-ObjectiveCLWrapperLink.h
@@ -9,6 +9,6 @@ void
 objcl_shutdown_instance_wrappers (void);
 
 @interface NSObject (ObjectiveCLWrapperLink)
--(BOOL) __objcl_isBackedByLispInstance;
--(void) __objcl_setBackedByLispInstance: (BOOL)backed_p;
++(BOOL) __objcl_isBackedByLispClass;
++(void) __objcl_setBackedByLispClass: (BOOL)backed_p;
 @end /* NSObject (ObjectiveCL) */
